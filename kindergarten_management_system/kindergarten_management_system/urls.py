dc4a1570f12cd5caf2945570da9165a2d8faf387
Hristo Gatsinski <gatsinski@gmail.com>
2017-03-25 19:34:50 +0200

    Basic Django CMS setup

--- kindergarten_management_system/kindergarten_management_system/urls.py
+++ kindergarten_management_system/kindergarten_management_system/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     # Examples:
@@ -7,4 +9,8 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('cms.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
