c6b0680b9056713876205a5eaa2f7c69e28189be
Hristo Gatsinski <gatsinski@gmail.com>
2017-05-20 19:39:29 +0300

    Add export functionality to `parents` app

--- kindergarten_management_system/kindergarten_management_system/contrib/parents/admin.py
+++ kindergarten_management_system/kindergarten_management_system/contrib/parents/admin.py
@@ -1,11 +1,28 @@
 from django.contrib import admin
 
+from import_export.resources import ModelResource
+from import_export.admin import ExportMixin
+
 from kindergarten_management_system.contrib.parents.models import \
     Parent
 
 
+class ParentResource(ModelResource):
+
+    class Meta:
+        model = Parent
+        fields = ('username',
+                  'first_name',
+                  'middle_name',
+                  'last_name',
+                  'email',
+                  'address',
+                  'telephone',)
+
+
 @admin.register(Parent)
-class ParentAdmin(admin.ModelAdmin):
+class ParentAdmin(ExportMixin, admin.ModelAdmin):
     list_display = ('first_name',
                     'middle_name',
                     'last_name')
+    resource_class = ParentResource
