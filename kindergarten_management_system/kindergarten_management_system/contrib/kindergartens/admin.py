ee70e98303bf11178693c9aecfbbdcd85ecc67b0
Hristo Gatsinski <gatsinski@gmail.com>
2017-05-20 19:39:17 +0300

    Add export functionality to `kindergartens` app

--- kindergarten_management_system/kindergarten_management_system/contrib/kindergartens/admin.py
+++ kindergarten_management_system/kindergarten_management_system/contrib/kindergartens/admin.py
@@ -1,12 +1,31 @@
 from django.contrib import admin
 
+from import_export.resources import ModelResource
+from import_export.admin import ImportExportModelAdmin, ExportMixin
+
 from kindergarten_management_system.contrib.kindergartens.models import \
     Kindergarten, KindergartenType, City
 
 
+class KindergartenResource(ModelResource):
+
+    class Meta:
+        model = Kindergarten
+        fields = ('name',
+                  'type',
+                  'city')
+
+    def dehydrate_type(self, kindergarten):
+        return kindergarten.type.name
+
+    def dehydrate_city(self, kindergarten):
+        return kindergarten.city.name
+
+
 @admin.register(Kindergarten)
-class KindergartenAdmin(admin.ModelAdmin):
+class KindergartenAdmin(ExportMixin, admin.ModelAdmin):
     list_display = ('name', 'type', 'city')
+    resource_class = KindergartenResource
 
 
 @admin.register(KindergartenType)
@@ -15,5 +34,5 @@ class KindergartenTypeAdmin(admin.ModelAdmin):
 
 
 @admin.register(City)
-class CityAdmin(admin.ModelAdmin):
+class CityAdmin(ImportExportModelAdmin):
     pass
