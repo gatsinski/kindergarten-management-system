e0b1b21c0ec3758fb63fbacbe10c3023ed434a81
Hristo Gatsinski <gatsinski@gmail.com>
2017-05-21 11:28:45 +0300

    Modify `send_staff_email` and `send_client_email` methods to use `send_html_email` function

--- kindergarten_management_system/kindergarten_management_system/contrib/applications/models.py
+++ kindergarten_management_system/kindergarten_management_system/contrib/applications/models.py
@@ -2,10 +2,9 @@ from django.db import models
 from django.utils.translation import ugettext as _
 from django.utils import timezone
 from django.utils.functional import cached_property
-from django.core.mail import EmailMultiAlternatives
-from django.template.loader import render_to_string
 from django.conf import settings
 
+from kindergarten_management_system.utils.emails import send_html_email
 from kindergarten_management_system.contrib.kindergartens.models import \
     Kindergarten
 from kindergarten_management_system.contrib.parents.models import Parent
@@ -50,22 +49,11 @@ class Application(models.Model):
             'date': self.date_created.strftime('%d.%m.%Y %H:%M'),
         }
 
-        html_content = \
-            render_to_string('applications/new_application_mail_staff.html',
-                             context)
-
-        text_content = \
-            render_to_string('applications/new_application_mail_staff.txt',
-                             context)
-
-        default_email = settings.DEFAULT_EMAIL
-
-        email = EmailMultiAlternatives(_('New application'),
-                                       text_content,
-                                       default_email,
-                                       [default_email])
-        email.attach_alternative(html_content, "text/html")
-        email.send(fail_silently=True)
+        send_html_email(_('New application'),
+                        'applications/new_application_mail_staff.txt',
+                        'applications/new_application_mail_staff.html',
+                        context,
+                        [settings.DEFAULT_EMAIL])
 
     def send_client_email(self):
         context = {
@@ -73,22 +61,11 @@ class Application(models.Model):
             'date': self.date_created.strftime('%d.%m.%Y %H:%M'),
         }
 
-        html_content = \
-            render_to_string('applications/new_application_mail_client.html',
-                             context)
-
-        text_content = \
-            render_to_string('applications/new_application_mail_client.txt',
-                             context)
-
-        default_email = settings.DEFAULT_EMAIL
-
-        email = EmailMultiAlternatives(_('Successfully submitted application'),
-                                       text_content,
-                                       default_email,
-                                       [self.parent.email])
-        email.attach_alternative(html_content, "text/html")
-        email.send()
+        send_html_email(_('New application'),
+                        'applications/new_application_mail_client.txt',
+                        'applications/new_application_mail_client.html',
+                        context,
+                        [self.parent.email])
 
 
 class Attachment(models.Model):
